f15befbf5f6c853345ed5d01217898e72f6e51b3
hejz <75412985@qq.com>
2019-02-15 12:21:05 +0800

    初始化

--- src/main/java/com/example/kafka/ShopService.java
+++ src/main/java/com/example/kafka/ShopService.java
@@ -3,23 +3,21 @@ package com.example.kafka;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 
 @RestController
 public class ShopService {
 
     @Autowired
-    private MessageChannel sendShopMessageChannel;
+    private ShopChannel shopChannel;
 
     @GetMapping("/sendMsg")
     public String sendShopMessage(String content) {
         System.out.println("sendMsg接收到消息");
-        boolean isSendSuccess = sendShopMessageChannel.
+        boolean isSendSuccess = shopChannel.sendShopMessage().
                 send(MessageBuilder.withPayload(content).build());
         return isSendSuccess ? "发送成功" : "发送失败";
     }
